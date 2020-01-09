6ebd1ddb3c089be63291b1ab6b632c2402b0ca82
Patrick Gläßer <p.glaesser@hotmail.de>
2020-01-10 00:03:04 +0100

    added logging of process.env.NODE_ENV

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,8 @@
 import logger from './logger';
 import dotenv from 'dotenv';
 
+logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+
 export const IsProductionEnvironment = process.env.NODE_ENV === 'production';
 
 if (!IsProductionEnvironment) {
